95a0333ba07010fd5d8b1c6ec49a97e833a85241
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-02 00:28:58 +0530

    Add : Compute Arithmetic a*b+c

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -3,6 +3,8 @@ echo "Welcome to ArithmeticComputation & sorting "
 # this operation ArithmeticComputation branch UC1
 read -p "Enter value for a : " a
 read -p "Enter value for b :" b
-read -p "Enter value for c :"c
+read -p "Enter value for c :" c
 # this operation ArithmeticComputation branch UC2
 echo $(($a+$b*$c))
+# here we are solving branch UC3
+echo $(($a*$b+$c))

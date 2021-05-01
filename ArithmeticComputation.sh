51b9f42e35662564d145e0354d3d27194866cf7d
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-01 23:37:35 +0530

    Add : UC2 Arithmetic a+b*c

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 echo "Welcome to ArithmeticComputation & sorting "
+# this operation ArithmeticComputation branch UC1
 read -p "Enter value for a : " a
 read -p "Enter value for b :" b
 read -p "Enter value for c :"c
+# this operation ArithmeticComputation branch UC2
+echo $(($a+$b*$c))
